a23e8bfe93361a42c6c8b8f818915a7c62f5077c
viktoriazaharova <zaharova181114@gmail.com>
2020-10-15 21:39:31 +0300

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -69,3 +69,50 @@ $('.btn-close, .overlay').click(function () {
     $('.mobile-menu').fadeOut();
     $('.overlay').fadeOut();
 });
+
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay, .btn-close-modal');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
+
+$('body').on('click', '.password-control', function(){
+    if ($('.input-password').attr('type') == 'password'){
+        $(this).addClass('view');
+        $('.input-password').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('.input-password').attr('type', 'password');
+    }
+    return false;
+});
